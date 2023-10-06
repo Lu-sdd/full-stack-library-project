f7a58780bd1675bda66bc05a1a2440f3d0bef1fc
Lu <l.han991230@gmail.com>
2023-10-06 19:18:01 +0100

    create ShelfCurrentLoans model, fetch user current loans from backend and render them on ShelfPage, create LoansModal

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -8,9 +8,10 @@ import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
-import { LoginCallback, Security } from '@okta/okta-react';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 // Create an OktaAuth instance using the provided Okta configuration.
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -65,6 +66,10 @@ export const App = () => {
             () => <LoginWidget config={oktaConfig} />
           } 
           />
+
+          <SecureRoute path='/shelf'>
+            <ShelfPage/>
+          </SecureRoute>
 
           <Route path = '/login/callback' component={LoginCallback} />
         </Switch>
--- frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom"
 import { useOktaAuth } from "@okta/okta-react"
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { link } from "fs";
 
 export const Navbar = () => {
 
@@ -36,6 +37,12 @@ export const Navbar = () => {
                         <li className='nav-item'>
                             <NavLink className='nav-link' to='/search'>Search Books</NavLink>
                         </li>
+
+                        {authState.isAuthenticated && 
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to='/shelf'>Shelf</NavLink> 
+                            </li>
+                        }
                     </ul>
 
                     <ul className='navbar-nav ms-auto'>
