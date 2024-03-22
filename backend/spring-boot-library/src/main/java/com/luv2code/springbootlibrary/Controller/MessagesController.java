1b1f51e52df01d2bd44948f1a4a041c1280d34f9
Lu <l.han991230@gmail.com>
2024-03-22 17:46:43 +0000

    library services spring boot

--- backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/Controller/MessagesController.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/Controller/MessagesController.java
@@ -1,2 +1,28 @@
-package com.luv2code.springbootlibrary.Controller;public class MessagesController {
+package com.luv2code.springbootlibrary.Controller;
+
+import com.luv2code.springbootlibrary.service.MessageService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.luv2code.springbootlibrary.entity.Message;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public MessagesController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value="Authorization") String token,
+                            @RequestBody Message messageRequest){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+
+    }
 }
--- backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessageService.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessageService.java
@@ -1,2 +1,24 @@
-package com.luv2code.springbootlibrary.service;public class MessageService {
+package com.luv2code.springbootlibrary.service;
+
+import com.luv2code.springbootlibrary.dao.MessageRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.luv2code.springbootlibrary.entity.Message;
+
+@Service
+@Transactional
+public class MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail){
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
 }
